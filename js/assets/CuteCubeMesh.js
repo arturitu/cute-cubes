72ddb2162978f2f07a0ebae2cfb418fa93dba89a
Unboring <arturo@unboring.net>
2016-03-31 05:42:29 +0200

    Fix mouths texture issue

--- js/assets/CuteCubeMesh.js
+++ js/assets/CuteCubeMesh.js
@@ -9,12 +9,12 @@ var CuteCubeMesh = function () {
 		var mat = new THREE.MultiMaterial( materials );
 		// cuteCube = new THREE.Mesh ( geometry, mat );
 		// console.log(mat.materials[1]);
-		THREE.Mesh.call( scope, geometry, mat );
 
 		var eyesTexture = new THREE.TextureLoader().load( 'assets/eyes.jpg', function() {
 
 			var mouthsTexture = new THREE.TextureLoader().load( 'assets/mouths.jpg', function() {
 
+				THREE.Mesh.call( scope, geometry, mat );
 				scope.dispatchEvent( { type: 'ready' } );
 
 			} );
